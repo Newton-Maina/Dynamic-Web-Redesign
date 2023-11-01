20af573c63f3402a23a4f579f61afbc7a2fc20fc
Newton Maina <newtonmainag@gmail.com>
2023-11-01 19:29:07 +0000

    Edits

--- sales/views.py
+++ sales/views.py
@@ -4,36 +4,36 @@ from django.shortcuts import render
 
 
 def index(request):
-     return render(request, 'index.html')
+     return render(request, 'index.html', {'navbar':'index'})
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'contact.html', {'navbar':'contact'})
 
 
 def checkout(request):
-    return render(request, 'checkout.html')
-
-
-def cart(request):
-    return render(request, 'cart.html')
+    return render(request, 'checkout.html',{'navbar':'checkout'})
 
 
 def blog(request):
-    return render(request, 'blog.html')
+    return render(request, 'blog.html',{'navbar':'blog'})
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {'navbar':'about'})
 
 
 def services(request):
-    return render(request, 'services.html')
+    return render(request, 'services.html',{'navbar':'services'})
 
 
 def shop(request):
-    return render(request, 'shop.html')
+    return render(request, 'shop.html',{'navbar':'shop'})
 
+def cart(request):
+    return render(request, 'cart.html',{'navbar':'cart'})
 
 def thankyou(request):
-    return render(request, 'thankyou.html')
+    return render(request, 'thankyou.html',{'navbar':'thankyou'})
+
+
